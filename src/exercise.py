3e060cf7c31f6076cdd5eb7a47013d414052b95b
Neix20 <66442152+Neix20@users.noreply.github.com>
2020-06-08 16:35:33 +0800

    Attempt 2

--- src/exercise.py
+++ src/exercise.py
@@ -1,7 +1,7 @@
 def main():
     #write your code below this line
     print("How many days would you like to convert to seconds?")
-    number=int(input('Give a number:'))
+    number=int(input())
     print(number*24*60*60)
 
 if __name__ == '__main__':
